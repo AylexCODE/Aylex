f28d844ce8d9715e26c0c5fb45d13f5aa1eb662c
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-23 04:28:31 +0000

    Initial side bar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,14 +6,17 @@ function Home(){
     const [navState, setNavState] = useState(false);
     
     return (
-        <main className="h-dvh w-dvw select-none text-textColor text-base">
-            <header className="h-[4rem] w-[100dvw] px-[1rem] shadow-[var(--color-navShadowColor)_0px_2px_8px] flex flex-row justify-between items-center">
+        <main className="h-dvh w-dvw select-none text-textColor text-base overflow-scroll bg-bgColor">
+            <header className="h-[4rem] w-dvw sticky top-0 px-[1rem] shadow-[var(--color-navShadowColor)_0px_2px_8px] flex flex-row justify-between items-center z-10">
                 <img src="%PUBLIC_URL%/../favicon.svg" alt="logo" className="overflow-hidden rounded-full h-[2.2rem] w-[2.2rem]"></img>
                 <button className="w-[2rem] h-[1.5rem]" onClick={() => { navState === false ? setNavState(true) : setNavState(false) }}>
                     <BurgerMenu state={navState} />
                 </button>
             </header>
-            <div></div>
+            <div className={"w-[70%] h-dvh fixed top-0 bg-bgColor rounded-r-xl z-20 transition-all duration-300 " +(navState ? "left-0" : "left-[-100dvw]")}>
+                
+            </div>
+            <div onClick={() => setNavState(!navState)} className={"fixed top-0 w-dvw h-dvh z-15 bg-sideBarCover transition-all duration-300 ease-in-out " +(navState ? "left-0" : "left-[-100dvw]")}></div>
             <aside></aside>
             <section className="h-[100dvh] w-[100dvw]"></section>
         </main>
