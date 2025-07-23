81702472bf9ef2c59bf3cba394d3324fec506cf1
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-23 02:08:04 +0000

    Added burger menu button

---
+++ src/pages/Home/components/menu/BurgerMenu.jsx
@@ -0,0 +1,13 @@
+import style from './menuStyle.module.css';
+
+export default function BurgerMenu(props){
+    return (
+        <>
+        <svg className={props.state ? style.activeMenu : "menu"} width="100%" height="100%" viewBox="0 0 65 48">
+            <path className={style.line1} fill="#000000" fill-opacity="0.0" stroke="#000000" stroke-width="6.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M3.0,3.0C3.0,3.0,74.15,3.0,60.18,3.0C60.18,3.0,51.06,3.0,51.06,3.0C51.06,3.0,32.42,23.98,32.42,23.98C32.42,23.98,51.09,44.92,51.09,44.92" stroke-linecap="round"/>
+            <path className={style.line2} fill="#000000" fill-opacity="0.0" stroke="#000000" stroke-width="6.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M4.01,24.03C4.01,24.03,60.99,24.17,60.99,24.17" stroke-linecap="round"/>
+            <path className={style.line3} fill="#000000" fill-opacity="0.0" stroke="#000000" stroke-width="6.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M61.84,44.92C61.84,44.92,-3.57,44.89,12.49,45.0C12.49,45.0,13.24,45.0,13.24,45.0C13.24,45.0,32.5,24.0,32.5,24.0C32.5,24.0,13.07,3.0,13.07,3.0" stroke-linecap="round"/>
+        </svg>
+        </>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,37 +1,21 @@
+import React, { useState } from 'react';
+
+import BurgerMenu from "./components/menu/BurgerMenu";
+
 function Home(){
+    const [navState, setNavState] = useState(false);
+    
     return (
         <main className="h-dvh w-dvw select-none text-textColor text-base">
-            <nav className="fixed top-0 flex flex-row justify-between items-center h-[40px] w-full px-[5px] shadow-[0px_1px_10px_0px] shadow-navShadowColor bg-navBg backdrop-blur-xs">
-                <span className="flex flex-column gap-[5px] items-center">
-                    <img src="%PUBLIC_URL%/../favicon.svg" height="30" width="30" className="rounded-sm overflow-hidden"></img>
-                    <p className="font-bold text-iconTextColor">Aylex</p>
-                </span>
-                <span className="flex flex-row gap-[10px] h-fit">
-                    <p>Projects</p>
-                    <p>About</p>
-                    <p>Home</p>
-                </span>
-            </nav>
-            <section id="home" className="h-dvh w-dvw flex flex-col items-center bg-bgOdd pt-[45px]">
-                <p>Home</p>
-            </section>
-            <section className="h-dvh w-dvw flex flex-col items-center bg-bgEven pt-[45px]">
-                <p>About</p>
-            </section>
-            <section className="h-dvh w-dvw flex flex-col items-center bg-bgOdd pt-[45px]">
-                <p className="font-bold text-xl">Projects</p>
-                <div className="h-[90%] w-[90%] p-[10px] flex flex-row gap-[10px]">
-                    <span className="h-[200px] w-[200px] flex flex-col rounded-sm border border-white p-[5px]">
-                        <p>Online Karaoke</p>
-                        <a href="https://github.com/AylexCODE/Karaoke" className="underline">Source Code</a>
-                        <a href="jkaraoke.42web.io" className="underline">Demo</a>
-                    </span>
-                    <span className="h-[200px] w-[200px] flex flex-col rounded-sm border border-white p-[5px]">
-                        <p>Discord BOT</p>
-                        <a href="">Source Code</a>
-                    </span>
-                </div>
-            </section>
+            <header className="h-[4rem] w-[100dvw] px-[1rem] shadow-[var(--color-navShadowColor)_0px_2px_8px] flex flex-row justify-between items-center">
+                <img src="%PUBLIC_URL%/../favicon.svg" alt="logo" className="overflow-hidden rounded-full h-[2.2rem] w-[2.2rem]"></img>
+                <button className="w-[2rem] h-[1.5rem]" onClick={() => { navState === false ? setNavState(true) : setNavState(false) }}>
+                    <BurgerMenu state={navState} />
+                </button>
+            </header>
+            <div></div>
+            <aside></aside>
+            <section className="h-[100dvh] w-[100dvw]"></section>
         </main>
     );
 }
