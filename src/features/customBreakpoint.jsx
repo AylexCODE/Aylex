32945f0557042b58ef17de2e6b475169fab9b5b0
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-24 08:44:18 +0000

    Added custom ustom breakpoint
    Because for some reason tailwind css breakpoints does not work when deployed

---
+++ src/features/customBreakpoint.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export default function Breakpoints(cb) {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+
+    const handleResize = () => {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    };
+
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        if (windowSize.width < 640) {
+            cb(640);
+        } else if (windowSize.width < 768) {
+            cb(768);
+        } else if (windowSize.width < 1280) {
+            cb(1280);
+        }
+
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, [windowSize.width]);
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-
-import BurgerMenu from "./components/menu/BurgerMenu";
-import SideBar from "./components/sideBar/SideBar";
 import { Outlet } from 'react-router-dom';
 
+import Breakpoints from '../../features/customBreakpoint';
+
+import BurgerMenu from "./components/menu/BurgerMenu";
+import SideBar from "./components/sideBar/SideBar"
+
 function Home(){
+    const [breakpoint, setBreakpoint] = useState(0);
     const [navState, setNavState] = useState(false);
+
+    Breakpoints(setBreakpoint);
 
     function pageHandler(){
         setNavState(false);
     }
     
     return (
-        <main className="h-dvh w-dvw bg-bgColor font-textFont select-none text-textColor text-base overflow-scroll md:flex md:flex-row">
+        <main className="h-auto w-dvw bg-bgColor font-textFont select-none text-textColor text-base md:flex md:flex-row">
             <header className="h-[4rem] w-dvw bg-componentsColor sticky top-0 px-[1rem] shadow-[var(--color-navShadowColor)_0px_2px_8px] flex flex-row justify-between items-center z-10 md:hidden">
                 <img src="%PUBLIC_URL%/../favicon.svg" alt="logo" className="overflow-hidden rounded-full h-[2.2rem] w-[2.2rem]"></img>
                 <button className="w-[2rem] h-[1.5rem]" onClick={() => { navState === false ? setNavState(true) : setNavState(false) }}>
@@ -26,7 +31,7 @@ function Home(){
             <aside className="max-md:hidden h-dvh w-[22rem] lg:w-[26rem] p-[1rem] pr-0">
                 <SideBar setPage={()=>false} />
             </aside>
-            <section className="h-dvh w-dvw md:w-full">
+            <section className="h-dvh w-dvw md:w-full">{breakpoint}
                 <Outlet />
             </section>
         </main>
