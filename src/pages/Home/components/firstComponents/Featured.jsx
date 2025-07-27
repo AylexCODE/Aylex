a6e877853d018748f6477947207d11130b74f824
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-27 11:32:04 +0000

    Hidden scrollbar

--- src/pages/Home/components/firstComponents/Featured.jsx
+++ src/pages/Home/components/firstComponents/Featured.jsx
@@ -1,7 +1,20 @@
 export default function Featured(props){
     return (
-        <div>
-            
+        <div className="pt-[1rem] w-full">
+            <ul className="w-[300px] h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:inline-block [&>li]:overflow-hidden [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>span>p]:font-medium">
+                <li>
+                    <div className="h-[15rem] w-[300px] bg-blue-500"></div>
+                    <span>
+                        <p>Discord BOT</p>
+                    </span>
+                </li>
+                <li>
+                    <div className="h-[15rem] w-[20rem] bg-green-500"></div>
+                    <span>
+                        <p>Online Karaoke</p>
+                    </span>
+                </li>
+            </ul>
         </div>
     )
 }
