2ab52c1bdea2fe6b57c60abd2fa93978c851070c
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-08-02 22:46:30 +0800

    Major improvements
    Added statistics e.g. longest streak, & improved overall UI

--- src/pages/Home/pages/projects/components/GithubContributions.jsx
+++ src/pages/Home/pages/projects/components/GithubContributions.jsx
@@ -5,17 +5,34 @@ export default function GithubContributions(){
     const [data, setData] = useState();
     const [contributions, setContributions] = useState();
     const [activeYear, setActiveYear] = useState(0);
-
+    const [statistics, setStatistics] = useState();
+    
     useEffect(() => {
         (async function(){
             const data = await new GithubGraphQL().getContributions();
             data.years.reverse();
             setData(data);
             
-            calculateContributions(data.data['y'+data.years[1]]);
+            calculateContributions(data, "Last Year");
         })();
         // eslint-disable-next-line
     }, []);
+    
+    function calculateLastYear(data){
+        let finalData = [];
+        const years2 = data.years.slice(1, 3).reverse();
+        
+        for(let i = 0; i < years2.length; i++){
+            for(const day of data.data['y'+years2[i]]){
+                finalData.push(day);
+            }
+        }
+        
+        const dataLength = finalData.length;
+        const excessDays = dataLength - 364;
+        
+        return finalData.slice(excessDays, dataLength);
+    }
     
     function getMonth(n){
         switch(n){
@@ -35,38 +52,105 @@ export default function GithubContributions(){
         }
     }
     
-    function calculateContributions(data){
-        let currentMonth = null, currentWeek = 0;
-        const months = [];
+    function calculateContributions(data, index){
+        if(index === "Last Year"){
+            data = calculateLastYear(data);
+        }else{
+            data = data.data['y'+index];
+        }
         
-        for(const day of data){
-            const monthInData = parseInt(day.date.split("-")[1]);
-            
-            if((currentMonth === monthInData || day.weekday) !== 0 && currentMonth){
-                if(day.weekday === 0){
-                    months[currentMonth][++currentWeek] = [day];
+        let currentMonth = null, currentWeek = 0, monthForLatest = 0, totalContribution = 0, firstMonth, streak = 0, longestStreak = 0, firstLongestStreak, lastLongestStreak, firstStreak, lastStreak, resetStreak = true, resetLongestStreak = true;
+        const months = [], possibleFirstLongestStreak = [];
+        
+        if(index === "Last Year"){
+            for(const day of data){
+                const monthInData = parseInt(day.date.split("-")[1]);
+                totalContribution += day.contributionCount; if(!firstMonth) firstMonth = day.date;
+                
+                if((currentMonth === monthInData || day.weekday) !== 0 && currentMonth){
+                    if(day.weekday === 0){
+                        months[monthForLatest][++currentWeek] = [day];
+                    }else{
+                        months[monthForLatest][currentWeek].push(day);
+                    }
                 }else{
-                    months[currentMonth][currentWeek].push(day);
+                    currentMonth = monthInData; monthForLatest++;
+                    currentWeek = 0;
+                    months[monthForLatest] = [];
+                    months[monthForLatest][currentWeek] = [day];
                 }
-            }else{
-                currentMonth = monthInData;
-                currentWeek = 0;
-                months[currentMonth] = [];
-                months[currentMonth][currentWeek] = [day];
+                
+                if(day.contributionCount != 0){
+                    streak++;
+                    lastStreak = day.date;
+                    if(resetStreak){
+                        firstStreak = day.date;
+                        resetStreak = false;
+                        possibleFirstLongestStreak.push(firstStreak);
+                    }
+                    if(resetLongestStreak){
+                        firstLongestStreak = day.date;
+                        resetLongestStreak = false;
+                    }
+                }else{
+                    if(streak > longestStreak){
+                        longestStreak = streak;
+                        lastLongestStreak = day.date;
+                        resetLongestStreak = true;
+                    }
+                    streak = 0;
+                    resetStreak = true;
+                }
             }
-            
-            //if(maxContributionCount < day.contributionCount) maxContributionCount = day.contributionCount;
+        }else{
+            for(const day of data){
+                const monthInData = parseInt(day.date.split("-")[1]);
+                totalContribution += day.contributionCount; if(!firstMonth) firstMonth = day.date;
+                
+                if((currentMonth === monthInData || day.weekday) !== 0 && currentMonth){
+                    if(day.weekday === 0){
+                        months[currentMonth][++currentWeek] = [day];
+                    }else{
+                        months[currentMonth][currentWeek].push(day);
+                    }
+                }else{
+                    currentMonth = monthInData;
+                    currentWeek = 0;
+                    months[currentMonth] = [];
+                    months[currentMonth][currentWeek] = [day];
+                }
+            }
+        }
+        //if(maxContributionCount < day.contributionCount) maxContributionCount = day.contributionCount;
+        const lastMonth = months[months.length-1][months[months.length-1].length-1][months[months.length-1][months[months.length-1].length-1].length-1].date;
+        if(streak > longestStreak){
+            longestStreak = streak;
+            lastLongestStreak = lastMonth;
+            possibleFirstLongestStreak.push(lastLongestStreak);
         }
         
         setContributions(months);
-        console.log(contributions);
+        if(index === "Last Year"){
+            firstLongestStreak = possibleFirstLongestStreak[possibleFirstLongestStreak.indexOf(lastLongestStreak)-1];
+            
+            setStatistics([totalContribution, firstMonth, lastMonth, longestStreak, firstLongestStreak, lastLongestStreak, streak, firstStreak, lastStreak]);
+        }else{
+            setStatistics([totalContribution, firstMonth, lastMonth, statistics[3], statistics[4], statistics[5], statistics[6], statistics[7], statistics[8]]);
+        }
     }
     
+    function formatDate(date){
+        const year = date.split("-")[0];
+        const month = date.split("-")[1];
+        const day = date.split("-")[2];
+        
+        return `${getMonth(month)} ${parseInt(day)}, ${year}`;
+    }
     return (
         <div className="h-fit w-full p-[16px] overflow-scroll text-[14px] border border-borderColor rounded-2xl">
         {data ? (
         <div className="w-fit">
-            <div className="w-fit flex flex-row gap-[16px]">
+            <div className="w-[919px] flex flex-row gap-[16px]">
                 <div className={`w-fit flex flex-row gap-[4px] [&>span]:flex [&>span]:flex-col [&>span]:gap-[4px] [&>span>p]:text-center [&>span>span]:flex [&>span>span]:flex-row [&>span>span]:gap-[4px] [&>span>span]:justify-end [&>span]:first:[&>span]:items-end [&>span>span>ul]:flex [&>span>span>ul]:flex-col [&>span>span>ul]:gap-[4px] [&>span>span>ul>li]:size-[12px] [&>span>span>ul>li]:inset-shadow-[0px_0px_0px_1px_var(--color-navShadowColor)] [&>span>span>ul>li]:rounded-xs`}>
                     {contributions.map((months) => (
                         <span key={'c'+months[0][0].date}>
@@ -84,18 +168,42 @@ export default function GithubContributions(){
                 </div>
                 <div className="h-[160px] py-[8px]">
                     <ul className="h-full w-[75px] flex flex-col overflow-scroll gap-[8px] text-[12.8px] [&>li]:border [&>li]:border-borderColor [&>li]:p-[4px] [&>li]:rounded-lg [&>li]:text-center [&>li]:shadow-[0px_2px_0px_var(--color-navShadowColor)] [&>.active]:shadow-[0px_2px_0px_var(--color-activeBtnShadowColor)] [&>.active]:bg-activeBtnColor [&>.active]:border-activeBtnColor [&>.active]:text-componentsColor [&>.active]:font-medium">{data.years.map((year, index) => (
-                        <li className={activeYear === index ? "active" : "inactive" } key={year}><button onClick={() => { calculateContributions(data.data['y'+data.years[index]]); setActiveYear(index); }}>{year}</button></li>
+                        <li className={activeYear === index ? "active" : "inactive" } key={year}><button onClick={() => { calculateContributions(data, data.years[index]); setActiveYear(index); }}>{year}</button></li>
                     ))}</ul>
                 </div>
             </div>
-            <div className="w-full bg-green-500 flex flex-row gap-[16px]">
-                <span className="w-[calc(100%-90px)] h-[16px] relative top-[-20px] flex flex-row justify-end items-center gap-[8px] [&>ul]:flex [&>ul]:flex-row [&>ul]:gap-[2px] [&>ul>li]:rounded-xs [&>ul>li]:size-[12px]">
-                    <p>Less</p>
-                    <ul>{data.colors.map((color) => (
-                        <li key={color} style={{backgroundColor: color}}></li>
-                    ))}</ul>
-                    <p>More</p>
+            <div className="w-full flex flex-col gap-[16px] h-[116px]">
+                <span className="w-[calc(100%-90px)] h-[16px] relative top-[-20px] flex flex-row justify-between items-center gap-[8px] [&>span>ul]:flex [&>span>ul]:flex-row [&>span>ul]:gap-[2px] [&>span>ul>li]:rounded-xs [&>span>ul>li]:size-[12px]">
+                    <span>
+                        <p>Total Contributions {data.totalContributions}</p>
+                    </span>
+                    <span className="flex flex-row justify-between items-center gap-[8px]">
+                        <p>Less</p>
+                        <ul>{data.colors.map((color) => (
+                            <li key={color} style={{backgroundColor: color}}></li>
+                        ))}</ul>
+                        <p>More</p>
+                    </span>
                 </span>
+                <div className="w-full flex justify-center relative bottom-[16px]">
+                    <div className="w-full flex flex-row justify-around border border-borderColor rounded-lg h-[100px] p-[16px] [&>span]:w-full [&>span]:text-center [&>span]:overflow-scroll [&>span]:text-nowrap [&>span]:whitespace-nowrap [&>span>h4]:font-medium [&>span>h4]:text-[16px]">
+                        <span>
+                            <p>Contributions in the last year</p>
+                            <h4>{statistics[0]}</h4>
+                            <p>{formatDate(statistics[1])} - {formatDate(statistics[2])}</p>
+                        </span>
+                        <span className="border-x border-dashed border-borderColor">
+                            <p>Longest Streak</p>
+                            <h4>{statistics[3]} {statistics[3] > 1 ? "Days" : "Day"}</h4>
+                            <p>{formatDate(statistics[4])} - {formatDate(statistics[5])}</p>
+                        </span>
+                        <span>
+                            <p>Current Streak</p>
+                            <h4>{statistics[6]} {statistics[6] > 1 ? "Days" : "Day"}</h4>
+                            <p>{formatDate(statistics[7])} - {formatDate(statistics[8])}</p>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
         ) : (
