1f29246d85d1028f43c2c3a439037a736fa54669
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-08-08 16:27:28 +0000

    Fixing UI

--- src/pages/Home/pages/main/components/Featured.jsx
+++ src/pages/Home/pages/main/components/Featured.jsx
@@ -1,7 +1,7 @@
 export default function Featured(props){
     return (
         <div className="pt-[1rem] w-full">
-            <ul className="w-full snap-x snap-mandatory h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:snap-center [&>li]:block [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>img]:h-[15rem] [&>li>img]:w-[20rem] [&>li>img]:rounded-t-xl [&>li>span>p]:first:font-medium">
+            <ul className="w-full snap-x snap-mandatory h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:snap-center [&>li]:block [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>img]:block [&>li>img]:h-[15rem] [&>li>img]:w-[20rem] [&>li>img]:rounded-t-xl [&>li>span>p]:first:font-medium">
                 <li>
                     <img src="%PUBLIC_URL%/../DiscordBOT-Handle.webp" alt="project handle"></img>
                     <span>
